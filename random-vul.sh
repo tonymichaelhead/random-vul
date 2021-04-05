b3a23a2fc6d658957a6afd7af113d41af20d7578
tonymichaelhead <tonymichaelhead@gmail.com>
2021-04-05 14:24:40 -0700

    Cleanup commented code

---
+++ random-vul.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+IFS=$'\n' read -d '' -a lines < vul.tsv
+size=${#lines[@]}
+index=$(($RANDOM % $size))
+line=${lines[$index]}
+
+echo "$line" | awk -F'\t' '{ print $1" "$4":"$5 }'
+echo "================"
+echo "$line" | awk -F'\t' '{ print $6 }'
